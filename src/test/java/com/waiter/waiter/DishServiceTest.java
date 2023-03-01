522e3ec0a0009cc9092e02903079a8bdfc4f80e2
lenovo <ntodor358@gmail.com>
2023-03-01 11:11:02 +0200

    test saveDish(@Valid Dish dish, BindingResult bindingResult)

---
+++ src/test/java/com/waiter/waiter/DishServiceTest.java
@@ -0,0 +1,54 @@
+package com.waiter.waiter;
+
+import com.waiter.waiter.entities.Dish;
+import com.waiter.waiter.repositories.DishRepository;
+import com.waiter.waiter.services.DishService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.validation.BindingResult;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class DishServiceTest {
+    @InjectMocks
+    private DishService dishService;
+
+    @Mock
+    private DishRepository dishRepository;
+
+    @Mock
+    private BindingResult bindingResult;
+
+    @BeforeEach
+    public void setUp(){
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void testSaveDishWithNoErrors() {
+        Dish dish = new Dish();
+        when(bindingResult.hasErrors()).thenReturn(false);
+        String result = dishService.saveDish(dish, bindingResult);
+        assertEquals("index", result);
+    }
+
+    @Test
+    public void testSaveDishWithErrors() {
+        Dish dish = new Dish();
+        when(bindingResult.hasErrors()).thenReturn(true);
+        String result = dishService.saveDish(dish, bindingResult);
+        assertEquals("/menu/add-dish", result);
+    }
+
+    @Test
+    public void testSaveDishSavesDish() {
+        Dish dish = new Dish();
+        when(bindingResult.hasErrors()).thenReturn(false);
+        dishService.saveDish(dish, bindingResult);
+        verify(dishRepository, times(1)).save(dish);
+    }
+}
