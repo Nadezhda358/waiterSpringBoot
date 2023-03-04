20ef4ba60a71e05a664a8c8327830b317050a292
lenovo <ntodor358@gmail.com>
2023-03-04 18:26:17 +0200

    added test for loadUserByUsername(String username) and isUserPresent(user)

--- src/test/java/com/waiter/waiter/UserDetailsServiceImplTest.java
+++ src/test/java/com/waiter/waiter/UserDetailsServiceImplTest.java
@@ -1,6 +1,7 @@
 package com.waiter.waiter;
 
 import com.waiter.waiter.entities.User;
+import com.waiter.waiter.enums.Role;
 import com.waiter.waiter.repositories.UserRepository;
 import com.waiter.waiter.services.UserDetailsServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,10 +10,13 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class UserDetailsServiceImplTest {
     @Mock
@@ -43,4 +47,58 @@ public class UserDetailsServiceImplTest {
         assertEquals("encodedPassword", user.getPassword());
         assertTrue(user.isEnabled());
     }
+    @Test
+    public void testIsUserPresentWithExistingUser() {
+        User user = new User();
+        user.setUsername("testuser");
+
+        Mockito.when(userRepo.getUserByUsername(user.getUsername())).thenReturn(user);
+
+        List<Object> result = userDetailsService.isUserPresent(user);
+
+        assertTrue((boolean) result.get(0));
+        assertEquals("Username Already Present!", result.get(1));
+    }
+
+    @Test
+    public void testIsUserPresentWithNonExistingUser() {
+        User user = new User();
+        user.setUsername("testuser");
+
+        Mockito.when(userRepo.getUserByUsername(user.getUsername())).thenReturn(null);
+
+        List<Object> result = userDetailsService.isUserPresent(user);
+
+        assertFalse((boolean) result.get(0));
+        assertNull(result.get(1));
+    }
+
+    @Test
+    public void testLoadUserByUsernameWithExistingUser() {
+        User user = new User();
+        user.setUsername("testuser");
+        user.setPassword("testpassword");
+        user.setEnabled(true);
+        user.setRole(Role.WAITER);
+
+        Mockito.when(userRepo.getUserByUsername(user.getUsername())).thenReturn(user);
+
+        UserDetails result = userDetailsService.loadUserByUsername(user.getUsername());
+
+        assertEquals(user.getUsername(), result.getUsername());
+        assertEquals(user.getPassword(), result.getPassword());
+        assertTrue(result.isEnabled());
+        assertEquals(user.getRole().toString(), result.getAuthorities().iterator().next().getAuthority());
+    }
+
+    @Test
+    public void testLoadUserByUsernameWithNonexistentUser() {
+        String username = "nonexistentuser";
+        Mockito.when(userRepo.getUserByUsername(username)).thenReturn(null);
+
+        Throwable exception = assertThrows(UsernameNotFoundException.class,
+                () -> userDetailsService.loadUserByUsername(username));
+        assertEquals("User not found!", exception.getMessage());
+    }
+
 }
