0b5b4731dba3e3251ee57a473682fcf87f115553
lenovo <ntodor358@gmail.com>
2023-03-08 10:32:20 +0200

    fixed drink modals

--- src/main/java/com/waiter/waiter/services/OrderService.java
+++ src/main/java/com/waiter/waiter/services/OrderService.java
@@ -2,10 +2,12 @@ package com.waiter.waiter.services;
 
 import com.waiter.waiter.entities.Order;
 import com.waiter.waiter.entities.RestaurantTable;
+import com.waiter.waiter.enums.OrderStatus;
 import com.waiter.waiter.repositories.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Optional;
 
 @Service
