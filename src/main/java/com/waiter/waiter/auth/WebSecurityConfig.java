6f61730226351dc5a00b78ba447d71dece4e2710
lenovo <ntodor358@gmail.com>
2023-03-15 18:19:29 +0200

    worked on design

--- src/main/java/com/waiter/waiter/auth/WebSecurityConfig.java
+++ src/main/java/com/waiter/waiter/auth/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -52,6 +53,10 @@ public class WebSecurityConfig  extends WebSecurityConfigurerAdapter {
                 .formLogin().loginPage("/login").permitAll()
                 .and()
                 .logout().permitAll();
+    }
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/pictures/**","/js/**","/css/**","/webjars/**");
     }
 
 }
